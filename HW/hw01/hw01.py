2c8a3562088b599e410e817147dded462186b30d
lee <2108074514@qq.com>
2026-07-17 10:54:16 +0800

    完成 hw01 所有测试

--- HW/hw01/hw01.py
+++ HW/hw01/hw01.py
@@ -13,9 +13,9 @@ def a_plus_abs_b(a, b):
     3
     """
     if b < 0:
-        f = _____
+        f = lambda x,y: x-y
     else:
-        f = _____
+        f = lambda x,y: x+y
     return f(a, b)
 
 def a_plus_abs_b_syntax_check():
@@ -42,7 +42,7 @@ def two_of_three(i, j, k):
     >>> two_of_three(5, 5, 5)
     50
     """
-    return _____
+    return i*i + j*j + k*k - max(i, j, k)**2
 
 def two_of_three_syntax_check():
     """Check that your two_of_three code consists of nothing but a return statement.
@@ -66,7 +66,9 @@ def largest_factor(n):
     1
     """
     "*** YOUR CODE HERE ***"
-
+    for i in range(2, n+1):
+        if n % i == 0:
+            return n // i
 
 def hailstone(n):
     """Print the hailstone sequence starting at n and return its
@@ -88,4 +90,13 @@ def hailstone(n):
     1
     """
     "*** YOUR CODE HERE ***"
-
+    length = 0
+    while n != 1:
+        print(n)
+        length += 1
+        if n%2 == 0:
+            n //= 2
+        else:
+            n = n*3 + 1
+    print(n)
+    return length + 1
